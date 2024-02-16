619552ca970bcd8d989c4aa8ad806312ddaabd6f
Artem K <artiukraineyt@gmail.com>
2024-02-16 14:44:44 +0100

    vStyles

--- Calculator_design/MainWindow.xaml.cs
+++ Calculator_design/MainWindow.xaml.cs
@@ -19,6 +19,18 @@ namespace Calculator_design
         public MainWindow()
         {
             InitializeComponent();
+            List<string> styles = new List<string> { "light", "dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "dark";
+        }
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = styleBox.SelectedItem as string;
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
         }
     }
 }
